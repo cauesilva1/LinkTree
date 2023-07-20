eb67f76f0b825e2979e905876994081ec64d5d76
Cauê catone silva <cauecatonesilva@gmail.com>
2023-07-20 11:48:18 -0400

    LinkTree para instagram

---
+++ index.js
@@ -0,0 +1,16 @@
+function changeBackground() {
+
+  let body = document.getElementsByTagName("body")[0];
+  let h2 = document.getElementById("h2");
+
+  let isChecked = document.querySelector('.switch-container input').checked;
+  if (isChecked) {
+    h2.style.color = "#fff";
+    body.style.backgroundImage = "url('./image/espaco.gif')";
+  } else {
+    h2.style.color = "#000000";
+    body.style.backgroundImage = "url('./image/light.gif')";
+    body.style.backgroundRepeat = "no-repeat";
+    body.style.backgroundSize = "cover";
+  }
+}
